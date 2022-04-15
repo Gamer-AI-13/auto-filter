703a7f5c2c37a8f3b67a274d187252c646147af0
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2022-04-15 23:57:31 +0530

    test

--- bot/plugins/Callbacks/About Btn/abt_main_cb.py
+++ bot/plugins/Callbacks/About Btn/abt_main_cb.py
@@ -32,14 +32,12 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2">Source</a>"""
 
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    '🛠 Updates Channel 🛠', url='https://t.me/+1D7BAGMgGwkxYTA1'
                 ),
                 
             InlineKeyboardButton
