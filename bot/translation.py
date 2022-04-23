24fcb341085696c5ecf167e5af9876557031347c
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2022-04-24 00:18:54 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -18,3 +18,4 @@ Here You Can Request Movie's, Just Sent Movie Name With Proper Google Spelling..
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
 <b>➥ Language</b> : <code>Python3</code>
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+"""
