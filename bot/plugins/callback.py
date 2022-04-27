a013f7670e098f40397075166208c01370dba7d5
Gamer-AI-13 <67921075+Gamer-AI-13@users.noreply.github.com>
2022-04-28 00:41:24 +0530

    update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1637,7 +1637,7 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            nlineKeyboardButton('🛠 Updates Channel 🛠', url='https://t.me/+1D7BAGMgGwkxYTA1')
+            lineKeyboardButton('🛠 Updates Channel 🛠', url='https://t.me/+1D7BAGMgGwkxYTA1')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
